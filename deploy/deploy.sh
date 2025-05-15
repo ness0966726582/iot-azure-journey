2f23e2fdb2b8d3ae79793143a7df4868e9ca124b
NESSHUANG <46336637+ness0966726582@users.noreply.github.com>
2025-05-15 12:09:44 +0800

    Delete deploy directory

--- deploy/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-az group create --name iot-demo --location eastasia
-az storage account create --name iotstor123456 --resource-group iot-demo --location eastasia --sku Standard_LRS
-az functionapp create --resource-group iot-demo --consumption-plan-location eastasia \
-  --runtime python --functions-version 4 --name my-iot-func123 \
-  --storage-account iotstor123456
-az storage table create --name devices --account-name iotstor123456 --auth-mode login
